d54c984643204f11de7a98f45020c46afc888c4b
Ryan de Gruyter <ryan.degruyter@kbc.be>
2019-09-25 20:10:30 +0200

    initial commit

---
+++ src/main/java/be/ordina/jworks/tournament/registration/RegistrationApplication.java
@@ -0,0 +1,11 @@
+package be.ordina.jworks.tournament.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationApplication.class, args);
+    }
+}
